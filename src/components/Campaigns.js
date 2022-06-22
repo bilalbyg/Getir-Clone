60d210158e3a1bc4962bde2454e3d7205c1cf2c3
bilalbyg <bbeygo.71@gmail.com>
2022-06-22 23:00:52 +0300

    campaigns completed

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Slider from "react-slick";
+import Banners from "../api/banners.json";
+import Title from "./ui/Title";
+import { IoIosArrowBack } from 'react-icons/io';
+import { IoIosArrowForward } from 'react-icons/io';
+
+function NextBtn(className, style, onClick) {
+  return (
+    <button className={"!text-brand-color absolute top-1/2 -right-6 -translate-y-1/2"} onClick={onClick}>
+      <IoIosArrowForward  size={22}/>
+    </button>
+  );
+}
+
+function PrevBtn(className, style, onClick) {
+  return (
+    <button className={"absolute top-1/2 -left-6 -translate-y-1/2 !text-brand-color"} onClick={onClick}>
+      <IoIosArrowBack size={22}/>
+    </button>
+  );
+}
 
 export default function Campaigns() {
+  const [banners, setBanners] = useState([]);
+
+  useEffect(() => {
+    setBanners(Banners);
+  }, []);
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 700,
+    autoplaySpeed: 3000,
+    nextArrow: <NextBtn />,
+    prevArrow: <PrevBtn />,
+    cssEase: "linear",
+  };
+
   return (
-    <div>Campaigns</div>
-  )
+    <div className="container mx-auto py-8">
+      <Title>Kampanyalar</Title>
+      <Slider className="flex gap-x-4" {...settings}>
+        {banners.length &&
+          banners.map((banner, index) => (
+            <div className="" key={banner.id}>
+              <picture className="block px-2">
+                <img className="rounded-lg" src={banner.image} />
+              </picture>
+            </div>
+          ))}
+      </Slider>
+    </div>
+  );
 }
--- src/components/ui/Category.js
+++ src/components/ui/Category.js
@@ -5,7 +5,7 @@ export default function Category({category : {id, title, image}}) {
     <a href="#" className="flex flex-col items-center text-center text-gray-500 p-4 gap-y-2 hover:bg-gray-300 
                         hover:text-primary-brand-color transition-all">
         <img src={image} alt={title} className="w-12 h-12 rounded-lg border border-gray-200"/>
-        <span className="text-sm font-semibold mt-3 tracking-tight">{title}</span>
+        <span className="text-sm font-semibold mt-3 tracking-tight whitespace-nowrap">{title}</span>
     </a>
   )
 }
