18e66b0ed28e24502686ea7ebe4034955762b7b7
bilalbyg <bbeygo.71@gmail.com>
2022-06-23 20:09:50 +0300

    mobile app banner completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'brand-color':'#4c3398',
-        'primary-brand-color':'#7849f7',
-        'second-brand-color':'#5d3ebc',
-        'brand-yellow':'#ffd300'
-      }
+      colors: {
+        "brand-color": "#4c3398",
+        "primary-brand-color": "#7849f7",
+        "second-brand-color": "#5d3ebc",
+        "brand-yellow": "#ffd300",
+      },
+      backgroundImage: {
+        "mobile-app": "url(https://getir.com/_next/static/images/doodle-d659f9f1fd505c811c2331fe3ffddd5f.png)",
+      },
     },
+    plugins: [],
   },
-  plugins: [],
-}
+};
